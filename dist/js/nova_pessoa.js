9f07558fa77d0c9c816532e7ca1923fa7301e6b1
Wesley Ozebe <wesleysantos345@hotmail.com>
2022-04-13 21:58:27 -0300

    FRONTEND - Criado arquivo JS e arquivo HTML de nova_pessoa, para criar e persistir uma pessoa no banco, via API

---
+++ dist/js/nova_pessoa.js
@@ -0,0 +1,49 @@
+import alert from "./alert.js";
+
+var SERVER = 'localhost';
+var PORT = '8080';
+
+async function salvaPessoa(){
+    var nome = document.querySelector("#nome").value;
+    var cpf = document.querySelector("#cpf").value;
+    var data_nasc  = document.querySelector("#data_nasc").value;
+    var peso = document.querySelector("#peso").value;
+    var altura = document.querySelector("#altura").value;
+    var sexo = document.querySelector("#sexo").value;
+    if(sexo = 'Masculino'){
+        sexo = 'M';
+    }else{
+        sexo = 'F';
+    }
+
+    if(nome == '' || cpf == '' || data_nasc == '' || peso == '' || altura == '' || sexo == 'Selecione'){
+        alert('Verifique os dados', 'danger');
+    }else{
+        var dados = JSON.stringify({nome: nome, cpf: cpf, data_nasc: data_nasc, peso: peso, altura: altura, sexo: sexo});
+        const pessoaPOST = await fetch(`http://${SERVER}:${PORT}/pessoas`, {method: 'POST', body: dados, mode: 'cors', headers: {"Content-Type": "application/json"}});
+        const response = await pessoaPOST.json();
+        
+        switch(response.erro){
+            case undefined:
+                alert(nome + ' inserido!', 'success');
+                document.querySelector("#nome").value = '';
+                document.querySelector("#cpf").value = '';
+                document.querySelector("#data_nasc").value = '';
+                document.querySelector("#peso").value = '';
+                document.querySelector("#altura").value = '';
+                document.querySelector("#sexo").value = 'Selecione';
+
+                break;
+            default: 
+                alert("Ocorreu um erro! " + 'Cód: ' +response.erro, 'danger');
+        }
+    }
+}
+
+//assim que carregar...
+window.onload = function() {
+    document.getElementById('form-salva').addEventListener('submit', function(event){
+        event.preventDefault()
+        salvaPessoa();
+      });
+  };
